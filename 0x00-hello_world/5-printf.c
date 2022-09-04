95950e9d18c501b337701828c383aba7d3b5aa2b
Miguel <dmiurgosmiguel@gmail.com>
2022-09-04 14:02:53 +0000

    add task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * The main function is the entry point
+ * Will return: 0 in case of success
+ *
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
