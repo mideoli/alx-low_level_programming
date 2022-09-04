9471aa7c4010b1857a4e970cefa9e11d91c8a0bf
Miguel <dmiurgosmiguel@gmail.com>
2022-09-04 13:47:11 +0000

    add task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
-
-/*
- * Tha main function is the entry point
- * Returns 0 in case of success 
- * Will return EOF if doesn't successed
+/**
+ *main function is the entry point
+ *Return 0 in case of success 
+ *Will return EOF if doesn't successed
  */
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
