fad9c44f2a71ec43a63e2ce73f0550c85a7b80af
Miguel <dmiurgosmiguel@gmail.com>
2022-09-13 22:32:11 +0000

    add task 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - prints the alphabet in lowercase
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int alphabet = 'a';
+	
+	while (alphabet <= 'z')
+	{
+		_putchar(alphabet);
+	}
+	_putchar('\n');
+	return (0);
+}
