7d2b71ea54c56b54a3794942680f783452b08fd1
Miguel <dmiurgosmiguel@gmail.com>
2022-09-14 18:05:53 -0700

    fix task 3-14

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -16,19 +16,15 @@ int main(void)
 	while (1)
 	{
 		fibsum = fib1 + fib2;
-
 		if (fibsum > 4000000)
 		{
 			break;
 		}
-
 		if ((fibsum % 2) == 0)
 		{
 			tot_sum += fibsum;
 		}
-
 		fib1 = fib2;
-
 		fib2 = fibsum;
 	}
 	printf("%.0f\n", tot_sum);
