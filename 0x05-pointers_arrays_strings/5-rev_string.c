3565bcbfa2fa887db041f0e8a566fa0206300ea7
Miguel <dmiurgosmiguel@gmail.com>
2022-09-20 01:59:30 +0000

    hot fix

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,11 +10,18 @@
 
 void rev_string(char *s)
 {
-	int len = strlen(s);
+	int len = 0;
+	char tmp;
+	int index = 0;
 	
-	while (len--)
+	while (s[index++])
 	{
-		_putchar(*(s + len));
+		len++;
 	}
-	_putchar (10);
+	for (index = len - 1; index >= len / 2; index--)
+	{
+		tmp = s[index];
+		s[index] = s[len - index - 1];
+		s[len - inex-1] = tmp;
+	}
 }
