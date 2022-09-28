24d27aa6321b830c51e388b941d6b48256615a0a
Miguel <dmiurgosmiguel@gmail.com>
2022-09-28 18:45:20 +0000

    minor changes to task 5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -15,8 +15,9 @@ char *_strstr(char *haystack, char *needle)
 	int i;
 
 	if (*needle == 0)
+	{
 		return (haystack);
-
+	}
 	while (*haystack)
 	{
 		i = 0;
@@ -25,15 +26,13 @@ char *_strstr(char *haystack, char *needle)
 		{
 			do {
 				if (needle[i + 1] == '\0')
+				{
 					return (haystack);
-
+				}
 				i++;
-
 			} while (haystack[i] == needle[i]);
 		}
-
 		haystack++;
 	}
-
 	return ('\0');
 }
