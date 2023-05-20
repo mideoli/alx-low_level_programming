0c2e1bac5796c99d4f49bef46c10d71245c9c4c3
Miguel de Oliveira <dmiurgosmiguel@gmail.com>
2023-05-20 04:35:55 +0200

    add task 9. Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point.
+ *
+ * Description: This program prints all possible combinations of single
+ *		digit numbers separated by comma (,).
+ *
+ * Return: 0 (Success).
+ *
+ */
+
+int main(void)
+{
+	int number = 0;
+
+	while (number < 10)
+	{
+		putchar(number + '0');
+		if (number == 9)
+		{
+			break;
+		}
+		putchar(',');
+		putchar(' ');
+		number++;
+	}
+	putchar('\n');
+
+	return 0;
+}
