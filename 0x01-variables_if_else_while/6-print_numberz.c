d636b4cd7818e95e2638a823a64045c3490ae79c
Miguel de Oliveira <dmiurgosmiguel@gmail.com>
2023-05-20 03:24:33 +0200

    add task 6. Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point.
+ *
+ * Description: This program prints all single digite numbers of base 10
+ * 		starting from 0.
+ *
+ * Return: 0 (Success).
+ *
+ */
+
+int main(void)
+{
+	int numberz = 0;
+
+	while (numberz < 10)
+	{
+		putchar(numberz + '0');
+		numberz++;
+	}
+	putchar('\n');
+
+	return (0);
+}
