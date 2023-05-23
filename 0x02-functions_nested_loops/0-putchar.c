d7436fb1d304b36f9554bf216818679a2171f607
Miguel de Oliveira <dmiurgosmiguel@gmail.com>
2023-05-24 01:45:52 +0200

    add task 0. _putchar: source code

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point.
+ *
+ * Description: This program prints: _putchar, followed by a new line.
+ *
+ * Return: 0 (Success).
+ *
+ */
+
+int main(void)
+{
+	printf("_putchar");
+
+	return (0);
+}
