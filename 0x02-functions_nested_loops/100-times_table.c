6dfc0c1546dc18d4edc3aa32878703dda1108e3e
Miguel de Oliveira <dmiurgosmiguel@gmail.com>
2023-05-25 04:44:53 +0200

    add advanced task 12. The World looks like a multiplication-table, or a mathematical equation, which, turn it how you will, balances itself: source code

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,56 @@
+#include "main.h"
+
+/**
+ * print_times_table - function that prints the n times table,
+ *			starting with 0.
+ *
+ * @n: value
+ */
+
+void print_times_table(int n)
+{
+	int num; /* number */
+	int mult; /* multiplier */
+	int prod; /* product */
+
+	if (n >= 0 && n <= 15)
+	{
+		num = 0;
+
+		while (num <= n)
+		{
+			_putchar('0');
+
+			mult = 1;
+
+			while (mult <= n)
+			{
+				_putchar(',');
+				_putchar(' ');
+				prod = num * mult;
+				if (prod <= 99)
+				{
+					_putchar(' ');
+				}
+				if (prod <= 9)
+				{
+					_putchar(' ');
+				}
+				if (prod >= 100)
+				{
+					_putchar((prod / 100) + '0');
+					_putchar(((prod / 10)) % 10 + '0');
+				}
+				else if (prod <= 99 && prod >= 10)
+				{
+					_putchar((prod / 10) + '0');
+				}
+				_putchar((prod % 10) + '0');
+
+				mult++;
+			}
+			num++;
+			_putchar('\n');
+		}
+	}
+}
