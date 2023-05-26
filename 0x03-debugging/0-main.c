f5a0ebaf12ef51a533f17f990b3f7dfc6ca7685e
Miguel de Oliveira <dmiurgosmiguel@gmail.com>
2023-05-26 02:22:24 +0200

    add task 0. Multiple mains: source code

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - Entry point.
+ * Description: This program test the function positive_or_negative()
+ *		gives the correct output when given case of 0.
+ *
+ * Return: 0 (Success).
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+	
+	return (0);
+}
