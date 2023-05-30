5ac772aa889c0b6b0116f683b8925f16a0970eb8
Miguel <dmiurgosmiguel@gmail.com>
2023-05-31 01:44:36 +0200

    fix task 0. 98 Battery st.: description for parameter 'n'

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -4,7 +4,7 @@
  * reset_to_98 - takes a pointer to an int as parameter and updates the
  * value it prints to 98.
  *
- * @n - pointer variable.
+ * @n: pointer variable to an int.
  */
 
 void reset_to_98(int *n)
