4fdc6c26af4a735fe4a4f60da88d589df249aefb
Miguel <dmiurgosmiguel@gmail.com>
2023-06-02 01:49:19 +0200

    add main.h: header file

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,22 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * 0x06. C - More pointers, arrays and strings: Header file.
+ * Contains prototypes of all functions developed on the project.
+ */
+
+int _putchar(char c);
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
+char *string_touper(char *);
+char *cap_string(char *);
+char *rot13(char *);
+void print_number(int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
+
+#endif /* MAIN_H */
