0cf437b6ef2fc8c880ca4dfd2b75551778c4a985
Miguel <dmiurgosmiguel@gmail.com>
2023-06-15 04:32:36 +0200

    fix task 1. The woman who has no imagination has no wings: output errors

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -2,12 +2,12 @@
 #include <stdlib.h>
 
 /**
- * _strdup - Returns a pointer to a newly allocated space in memory
+ * _strdup - Returns a pointer to a newly-allocated space in memory
  * containing a copy of the string given as parameter.
  * @str: The string to be copied.
  *
- * Return: If str == NULL or insufficient memory is available - (NULL).
- * Otherwise - a pointer to the duplicated string.
+ * Return: If str == NULL or insufficient memory is available - NULL.
+ * else - a pointer to the duplicated string.
  */
 char *_strdup(char *str)
 {
@@ -17,23 +17,16 @@ char *_strdup(char *str)
 	if (str == NULL)
 		return (NULL);
 
-	i = 0;
-	while (str[i])
-	{
+	for (i = 0; str[i]; i++)
 		len++;
-		i++;
-	}
 
 	duplicate = malloc(sizeof(char) * (len + 1));
 
 	if (duplicate == NULL)
 		return (NULL);
 
-	while (str[i])
-	{
+	for (i = 0; str[i]; i++)
 		duplicate[i] = str[i];
-		i++;
-	}
 
 	duplicate[len] = '\0';
 
