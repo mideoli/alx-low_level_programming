63c123ea9eea1b28e1ae3517811aa8441a7eda2a
Miguel <dmiurgosmiguel@gmail.com>
2023-06-20 01:42:21 +0200

    add task 2. File name: source code

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,18 @@
+#ifndef MAIN_C
+#define MAIN_C
+
+#include <stdio.h>
+
+/*
+ * main - prints the name of the file it was compiled from.
+ *
+ * Return: 0 (Success).
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
+
+#endif /* MAIN_C */
