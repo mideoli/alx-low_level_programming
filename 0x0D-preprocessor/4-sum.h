3347c4c2ff3a57af533961be06031bd2956ba793
Miguel <dmiurgosmiguel@gmail.com>
2023-06-20 02:27:21 +0200

    update task 4. SUM: description

--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -1,6 +1,12 @@
 #ifndef SUM_H
 #define SUM_H
 
+/**
+ * Header file.
+ * Description: function-like macro SUM(x, y) that computes the sum
+ * of numbers x and y.
+ */
+
 #define SUM(a, b) ((a) + (b))
 
 #endif /* SUM_H */
