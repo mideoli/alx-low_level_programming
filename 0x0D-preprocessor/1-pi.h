f6501ae84dce35e92f01c372b6ab0b196cb8ddea
Miguel <dmiurgosmiguel@gmail.com>
2023-06-20 02:29:28 +0200

    update task 1. Pi: description

--- 0x0D-preprocessor/1-pi.h
+++ 0x0D-preprocessor/1-pi.h
@@ -1,6 +1,11 @@
 #ifndef PI_H
 #define PI_H
 
+/**
+ * Description: Header file that defines a macro named PI as an abreviation
+ * for token 3.14159265359.
+ */
+
 #define PI 3.14159265359
 
 #endif /* PI_H */
