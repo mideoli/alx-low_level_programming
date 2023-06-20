1b1c5e0c85cd0a01594812c45f72bcdebd977eb7
Miguel <dmiurgosmiguel@gmail.com>
2023-06-20 23:55:54 +0200

    update task 0. Poppy: source code

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,4 +15,28 @@ struct dog
 	char *owner;
 };
 
+/* Prototypes */
+
+/**
+ * init_dog - function that initialize a variable of type struct dog.
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+/**
+ * print_dog - prints a struct dog.
+ */
+void print_dog(struct dog *d);
+
+/**
+ * dog_t new_dog - creates new dog.
+ */
+dog_t *new_dog(char *name, float age, char *owner);
+
+/**
+ * free_dog - frees dog.
+ */
+void free_dog(dog_t *d);
+
+/* End of prototypes */
+
 #endif /* DOG_H */
