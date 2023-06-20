9890ebef46243efce5c70f9ddbdad1ff7de8d80d
Miguel <dmiurgosmiguel@gmail.com>
2023-06-20 22:36:31 +0200

    add task 0. Poppy: source code

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Dog structure data type
+ * @name: stores the dog name
+ * @age: stores the dog age.
+ * @owner: store the owner's name
+ */
+
+struct dog {
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
