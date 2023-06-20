5be859c8ff5904e6c462051cbac57bd114ebaeb7
Miguel <dmiurgosmiguel@gmail.com>
2023-06-20 02:26:29 +0200

    add task 3. Function-like macro: source code

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,12 @@
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+
+/**
+ * Header file
+ * Description: function-like macro ABS(x) that computes the absolute
+ * value of a number x.
+ */
+
+#define ABS(x) ((x) < 0 ? ((-x)) : (x))
+
+#endif /* FUNCTION_LIKE_MACRO_H */
