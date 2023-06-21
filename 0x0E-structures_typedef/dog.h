5a4f3b164f9d1024381b4813030641703f485ecc
Miguel <dmiurgosmiguel@gmail.com>
2023-06-21 02:11:36 +0200

    update dog.h: source code

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,13 +1,16 @@
 #ifndef DOG_H
 #define DOG_H
 
-/**
- * struct dog - Dog structure data type
- * @name: stores the dog name
- * @age: stores the dog age.
- * @owner: store the owner's name
+/*
+ * Desc: Header file that defines a new type struct dog.
  */
 
+/**
+ * struct dog - A new type describing a dog.
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
+ */
 struct dog
 {
 	char *name;
@@ -15,8 +18,14 @@ struct dog
 	char *owner;
 };
 
+/**
+ * dog_t - Typedef for struct dog
+ */
 typedef struct dog dog_t;
 
+void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
-#endif /* DOG_H */
+#endif
